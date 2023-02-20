8b71c70727c850087215d3921e2ecdad45bd890a
Jonas Hoersch <jonas.hoersch@climateanalytics.org>
2023-02-20 19:23:22 +0100

    Simplify github workflows and consolidate dependencies

--- src/sampleproject/__init__.py
+++ src/sampleproject/__init__.py
@@ -1,3 +1,11 @@
 """Initial documentation of SampleProject."""
 
-from ._version import __version__
+from importlib.metadata import version as _version
+
+
+try:
+    __version__ = _version("jmct_sampleproject")
+except Exception:
+    # Local copy or not installed with setuptools.
+    # Disable minimum version checks on downstream libraries.
+    __version__ = "999"
